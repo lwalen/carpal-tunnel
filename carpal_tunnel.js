28b9266bac6b600bf75852e0c9f488ee2249f1bc
Lars Walen <lars@walen.me>
2015-03-23 19:24:29 -0700

    Got basic diseases page going

--- carpal_tunnel.js
+++ carpal_tunnel.js
@@ -1,6 +1,38 @@
 
-var carpalTunnel = angular.module('carpalTunnel', []);
+var carpalTunnel = angular.module('carpalTunnel', ['ngRoute']);
 
-carpalTunnel.controller('DiagnoseCtrl', function($scope, $http) {
+carpalTunnel.config(function($routeProvider) {
 
+	$routeProvider.
+		when('/', {
+			templateUrl: 'main.html',
+			controller: 'MainCtrl'
+		}).
+		when('/diseases', {
+			templateUrl: 'diseases-list.html',
+			controller: 'DiseasesListCtrl'
+		}).
+		otherwise({
+			redirectTo: '/'
+		});
+});
+
+carpalTunnel.controller('MainCtrl', function($scope, $http) {
+
+
+});
+
+carpalTunnel.controller('DiseasesListCtrl', function($scope, $http) {
+	$http.get('data.json').success(function(data) {
+		var categories = [];
+		for (var i = 0; i < data.length; i++) {
+			console.log(data[i].category);
+			if (data[i].category) {
+				categories[i] = data[i].category;
+			} else if (data[i].name) {
+				categories[i] = data[i].name;
+			}
+		}
+		$scope.categories = categories;
+	});
 });
